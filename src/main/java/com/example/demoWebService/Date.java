52f0e9cdc6a1907d93e2a99e322050740f9f0f59
Shad <65975514+FMaxime21@users.noreply.github.com>
2021-02-15 18:12:28 +0100

    Ajout des classes Car, CarService, CarServiceClient, Date, Personnes, Rent

---
+++ src/main/java/com/example/demoWebService/Date.java
@@ -0,0 +1,8 @@
+package com.example.demoWebService;
+
+public class Date {
+
+    private String begin;
+    private String end;
+
+}
---
+++ src/main/java/com/example/demoWebService/Personnes.java
@@ -0,0 +1,12 @@
+package com.example.demoWebService;
+
+public class Personnes
+{
+    private String plateNumber;
+    private Personnes person;
+
+    public void setNom(String milou)
+    {
+
+    }
+}
